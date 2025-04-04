542663fae5e18dfd6eba9e168fcaba50ea0a98d7
EmadZahran <108583594+EmadZahran@users.noreply.github.com>
2025-04-04 14:29:35 +0200

    Update App.jsx

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,77 +1,192 @@
-import { useState } from 'react'
-import Filter from './components/Filter'
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
+  import { useState , useEffect } from 'react'
+  import Filter from './components/Filter'
+  import PersonForm from './components/PersonForm'
+  import './index.css'
+  import Persons from './components/Persons'
+  import PersonsService from './services/persons'
+  const Notification = ({ deleteMessage, addMessage , changeMessage , removeMessage}) => {
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: '1' }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [newSearch, setNewSearch] = useState('')
+  if (deleteMessage){
+    return (
+      
+      <div className='delete'>
+        {deleteMessage}
+      </div>
+    )
+  } else if (addMessage){
+    return (
+    <div className='add'>
+        {addMessage}
+      </div>
 
-  const handleNewname = (event) => {
-    setNewName(event.target.value)
+    ) 
+  }else if (changeMessage){
+    return (
+    <div className='change'>
+        {changeMessage}
+      </div>
+
+    ) 
+  }else if (removeMessage){
+    return (
+    <div className='delete'>
+        {removeMessage}
+      </div>
+
+    ) 
   }
-
-  const handleNewNumber = (event) => {
-    setNewNumber(event.target.value)
+  return null; 
   }
+    
+  const App = () => {
+    const [persons, setPersons] = useState([])
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+    const [newSearch, setNewSearch] = useState('')
+    const [deleteMessage, setDeleteMessage] = useState('')
+    const [addMessage, setAddMessage] = useState('')
+    const [changeMessage, setChangeMessage] = useState('')
+    const [removeMessage, setRemoveMessage] = useState('')
+    const hook = () => {
+      PersonsService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+    })
 
-  const handleSearch = (event) => {
-    setNewSearch(event.target.value)
-  }
-
-  const existingPerson = persons.find((person) => person.name === newName || person.number === newNumber)
-
-  const add = (event) => {
-    event.preventDefault()
-    if(newName === '' || newNumber === ''){
-      alert(newName ? `You cannot leave the number field blank.` : 'You cannot leave the name field blank.')
-      return
     }
-    if (existingPerson){
-      alert(`${existingPerson.name} is already added to phonebook with ${existingPerson.number} number`)
-      return
+    
+  useEffect(hook, [])
+    console.log('render', persons.length, 'notes')
+    const handleNewname = (event) => {
+      setNewName(event.target.value)
     }
 
-    const nameObject = {
-      name: newName, 
-      number: newNumber,
-      id: String(persons.length + 1),
+    const handleNewNumber = (event) => {
+      setNewNumber(event.target.value)
     }
 
-    setPersons(persons.concat(nameObject))
-    setNewName('')
-    setNewNumber('')
+    const handleSearch = (event) => {
+      setNewSearch(event.target.value)
+    }
+
+    const existingPerson = persons.find((person) => person.name === newName)
+
+    const add = (event) => {
+      event.preventDefault()
+      if(newName === '' || newNumber === ''){
+        alert(newName ? `You cannot leave the number field blank.` : 'You cannot leave the name field blank.')
+        return
+      }
+      if (existingPerson){
+        if(existingPerson.number === newNumber){
+          alert('This data has been previously saved')
+          return
+        } else {
+
+        
+        const Numbertochange = persons.find(p => p.id === existingPerson.id)
+      
+        const changedNumber = { ...Numbertochange, number: newNumber }
+        if (window.confirm(`${existingPerson.name} is already added to phonebook with ${existingPerson.number} number, Do you want to replace it with the new number ${newNumber}?`)) {
+          
+            PersonsService
+            .update(Numbertochange.id, changedNumber)  
+            .then(returnedPerosn => {
+              setPersons(persons.map(person => person.id === Numbertochange.id ? returnedPerosn : person))
+          
+            setChangeMessage(
+              `The data for ${newName} has been changed`
+            )
+            setTimeout(() => {
+              setChangeMessage('')
+            }, 5000)
+            }).catch(error => {
+              setRemoveMessage(
+                `Information of ${Numbertochange.name} has already been removed from server`
+              )
+              setPersons(persons.filter(p => p.id !== Numbertochange.id))
+              setTimeout(() => {
+                setRemoveMessage('')
+              }, 5000)
+            })
+
+            
+        } 
+        return
+  }
+    
+      }
+
+      const nameObject = {
+        name: newName, 
+        number: newNumber,
+        id: String(persons.length + 1),
+      }
+      PersonsService
+      .create(nameObject)
+      .then(returnedNote => {
+      setPersons(persons.concat(returnedNote))
+      setNewName('')
+      setNewNumber('')
+    
+      
+
+      })
+      setAddMessage(
+        `${newName} has been added`
+      )
+      setTimeout(() => {
+        setAddMessage('')
+      }, 5000)
+      
+    }
+
+    const filteredPersons = persons.filter((person) =>
+      person.name.toLowerCase().includes(newSearch.toLowerCase())
+    )
+    const deletePerson = (id) => {
+      const persontodelete = persons.find(p => p.id === id)
+
+      if (window.confirm(`Delete ${persontodelete.name} ?`)) {
+        
+          PersonsService
+          .remove(persontodelete.id)  
+          .then(() => {
+            setPersons(persons.filter(p => p.id !== id))
+          
+          });
+          setDeleteMessage(
+            `${persontodelete.name} has been deleted`
+          )
+          setTimeout(() => {
+            setDeleteMessage('')
+          }, 5000)
+      }
+    }
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <Notification deleteMessage = {deleteMessage} addMessage= {addMessage} changeMessage = {changeMessage} removeMessage = {removeMessage}/>
+        <Filter onSearch={handleSearch} searchValue={newSearch} />
+       
+        <h3>Add a new</h3>
+
+        <PersonForm 
+          onNameChange={handleNewname}
+          onNumberChange={handleNewNumber}
+          newName={newName}
+          newNumber={newNumber}
+          onSubmit={add}
+        />
+
+        <h3>Numbers</h3>
+
+        <Persons persons={filteredPersons} deletePerson={deletePerson} />
+
+
+      </div>
+    )
   }
 
-  const filteredPersons = persons.filter((person) =>
-    person.name.toLowerCase().includes(newSearch.toLowerCase())
-  )
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-
-      <Filter onSearch={handleSearch} searchValue={newSearch} />
-
-      <h3>Add a new</h3>
-
-      <PersonForm 
-        onNameChange={handleNewname}
-        onNumberChange={handleNewNumber}
-        newName={newName}
-        newNumber={newNumber}
-        onSubmit={add}
-      />
-
-      <h3>Numbers</h3>
-
-      <Persons persons={filteredPersons} />
-    </div>
-  )
-}
-
-export default App
+  export default App
