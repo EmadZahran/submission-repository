b29e9ead6b34603a8a98be5c16dc972d6e8aa77a
EmadZahran <108583594+EmadZahran@users.noreply.github.com>
2025-04-04 14:30:31 +0200

    Update Persons.jsx

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,10 +1,14 @@
-import Phonebook  from '../components/Phonebook '
-
-const Persons = ({ persons }) => {
+import Phonebook  from './Phonebook'
+const Persons = ({ persons, deletePerson  }) => {
   return (
     <div>
-      {persons.map((person) => 
+      {persons.map((person) => (
+        <div key={person.id} style={{ display: 'flex', alignItems: 'center', marginBottom: '8px' }}>
         <Phonebook  key={person.id} name={person.name} number={person.number}/>
+        <button onClick={() => deletePerson(person.id)}>Delete</button>
+        </div>
+)
+
       )}
     </div>
   )
